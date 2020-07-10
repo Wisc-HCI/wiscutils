4b2e3a5c5c5cc7d8126c937271fd1c1d1b658375
Andrew Schoen <schoen.andrewj@gmail.com>
2020-07-10 15:07:03 -0500

    Update wisc_program.py

--- wisc_actions/wisc_actions/wisc_program.py
+++ wisc_actions/wisc_actions/wisc_program.py
@@ -2,7 +2,7 @@ import rclpy
 from rclpy.node import Node
 # from wisc_script.handling import Manager
 from flask import Flask
-from flask_pymongo import PyMongo
+# from flask_pymongo import PyMongo
 
 # This will probably all change.
 
@@ -13,11 +13,6 @@ class WiscProgramNode(Node):
     def __init__(self):
         super(WiscProgramNode,self).__init__('wisc_program')
         self.app = Flask(__name__)
-        self.app.config['MONGO_URI'] = 'mongodb+srv://admin:9VmeVmJFGxbDtKqB@programming-n2ugh.mongodb.net/programming?retryWrites=true&w=majority'
-        self.mongo = PyMongo(self.app)
-        self.db = self.mongo.db
-
-        #self.manager = Manager(self.db)
 
 
 def main(args=None):
